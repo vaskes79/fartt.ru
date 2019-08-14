e98f6d48a74f3d3b8ce4e691f4cef5c292e5d152
Vasily Guzov <vasily.guzov@gmail.com>
2019-08-14 16:34:15 +0300

    add lightbox

--- src/components/actor-component/ActorComponent.js
+++ src/components/actor-component/ActorComponent.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import Img from "gatsby-image"
 import ReactMarkdown from "react-markdown"
 import YouTube from "react-youtube-embed"
@@ -6,6 +6,8 @@ import dayjs from "dayjs"
 import Layout from "../layout"
 import Grid from "../grid"
 import AgentWidget from "../agent-widget"
+import Lightbox from "react-image-lightbox"
+import "react-image-lightbox/style.css" // This only needs to be imported once in your app
 import cn from "classnames"
 import "dayjs/locale/ru"
 import styles from "./ActorComponent.module.css"
@@ -14,6 +16,7 @@ dayjs.locale("ru")
 
 const {
   container,
+  lightBoxStyle,
   name,
   agentWidget,
   baseInfo,
@@ -23,6 +26,7 @@ const {
   titlePhoto,
   media,
   video,
+  photo,
 } = styles
 
 const ActorComponent = ({
@@ -30,6 +34,7 @@ const ActorComponent = ({
     placehoder,
     strapiActor: {
       name_ru,
+      name_en,
       birth_date,
       height,
       size,
@@ -39,65 +44,112 @@ const ActorComponent = ({
       drive_lisence,
       photos,
       content_ru,
+      content_en,
       agent,
     },
   },
-}) => (
-  <Layout>
-    <div className={container}>
-      <h1 className={name}>{name_ru}</h1>
-      <div className={titlePhoto}>
-        <Img fluid={title_photo.childImageSharp.fluid || placehoder.childImageSharp.fluid} />
-      </div>
-      <div className={agentWidget}>
-        <AgentWidget agent={agent} />
-      </div>
-      <div className={baseInfo}>
-        <ul className={baseInfoList}>
-          <li>
-            <em>возраст: </em>
-            <b>{dayjs(Date.now()).diff(birth_date, "year")}</b>
-          </li>
-          <li>
-            <em>рост: </em>
-            <b> {height}</b>
-          </li>
-          <li>
-            <em>размер: </em>
-            <b>{size}</b>
-          </li>
-          {drive_lisence ? (
-            <li>
-              <em>права:</em>
-              <b>есть</b>
-            </li>
-          ) : null}
-        </ul>
-      </div>
-      <div className={additionalInfo}>
-        {content_ru ? <ReactMarkdown escapeHtml={false} source={content_ru} /> : null}
-      </div>
-      <div className={media}>
-        <Grid>
-          {photos ? photos.map(({ url, id }) => <img key={id} src={url} alt={name_ru} />) : null}
-        </Grid>
-        <Grid column="twoColumn">
-          {video_intro ? (
-            <div className={cn("video", video)}>
-              <h2>видео визитка</h2>
-              <YouTube id={video_intro} />
-            </div>
-          ) : null}
-          {video_showreal ? (
-            <div className={video}>
-              <h2>Шоуреал</h2>
-              <YouTube id={video_showreal} />
-            </div>
-          ) : null}
-        </Grid>
-      </div>
-    </div>
-  </Layout>
-)
+}) => {
+  const [lightBoxIsOpen, openLightBox] = useState(false)
+  const [photoIndex, changeIndex] = useState(0)
+
+  const openCloseLightBox = () => {
+    openLightBox(!lightBoxIsOpen)
+  }
+
+  const handleChangeIndex = photoIndex => {
+    changeIndex(photoIndex)
+    !lightBoxIsOpen ? openLightBox(true) : null
+  }
+
+  const closeLightBox = () => {
+    openLightBox(false)
+    changeIndex(0)
+  }
+
+  return (
+    <>
+      <Layout>
+        <div className={container}>
+          <h1 className={name}>{name_ru || name_en}</h1>
+          <div className={titlePhoto} onClick={openCloseLightBox}>
+            <Img fluid={title_photo.childImageSharp.fluid || placehoder.childImageSharp.fluid} />
+          </div>
+          <div className={agentWidget}>
+            <AgentWidget agent={agent} />
+          </div>
+          <div className={baseInfo}>
+            <ul className={baseInfoList}>
+              <li>
+                <em>возраст: </em>
+                <b>{dayjs(Date.now()).diff(birth_date, "year")}</b>
+              </li>
+              <li>
+                <em>рост: </em>
+                <b> {height}</b>
+              </li>
+              <li>
+                <em>размер: </em>
+                <b>{size}</b>
+              </li>
+              {drive_lisence ? (
+                <li>
+                  <em>права:</em>
+                  <b>есть</b>
+                </li>
+              ) : null}
+            </ul>
+          </div>
+          <div className={additionalInfo}>
+            {content_ru ? <ReactMarkdown escapeHtml={false} source={content_ru} /> : null}
+          </div>
+          <div className={media}>
+            <Grid>
+              {photos
+                ? photos.map(({ url, id }, index) => (
+                    <img
+                      className={photo}
+                      key={id}
+                      src={url}
+                      alt={name_ru}
+                      onClick={() => handleChangeIndex(index)}
+                    />
+                  ))
+                : null}
+            </Grid>
+            <Grid column="twoColumn">
+              {video_intro ? (
+                <div className={cn("video", video)}>
+                  <h2>видео визитка</h2>
+                  <YouTube id={video_intro} />
+                </div>
+              ) : null}
+              {video_showreal ? (
+                <div className={video}>
+                  <h2>Шоуреал</h2>
+                  <YouTube id={video_showreal} />
+                </div>
+              ) : null}
+            </Grid>
+          </div>
+        </div>
+      </Layout>
+      {lightBoxIsOpen && (
+        <Lightbox
+          mainSrc={photos[photoIndex].url}
+          nextSrc={photos[(photoIndex + 1) % photos.length].url}
+          prevSrc={photos[(photoIndex + photos.length - 1) % photos.length]}
+          onCloseRequest={closeLightBox}
+          reactModalStyle={{ overlay: { zIndex: 1100 } }}
+          enableZoom={false}
+          wrapperClassName={lightBoxStyle}
+          onMovePrevRequest={() =>
+            handleChangeIndex((photoIndex + photos.length - 1) % photos.length)
+          }
+          onMoveNextRequest={() => handleChangeIndex((photoIndex + 1) % photos.length)}
+        />
+      )}
+    </>
+  )
+}
 
 export default ActorComponent
